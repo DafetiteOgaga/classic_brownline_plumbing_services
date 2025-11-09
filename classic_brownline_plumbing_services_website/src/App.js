7d3f6c0d5fd18c94e7cd23d80ddf9a7a38868e89
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-11-09 02:28:54 +0100

    App.js:

---
+++ classic_brownline_plumbing_services_website/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+// import "owl.carousel/dist/assets/owl.carousel.css";
+// import "owl.carousel/dist/assets/owl.theme.default.css";
+import './App.css';
+import './assets/css/bootstrap.min.css';
+import './assets/css/style.css';
+import './assets/css/animate.css';
+import { Index } from './components';
+
+function App() {
+  return (
+    <Index />
+  );
+}
+
+export default App;
