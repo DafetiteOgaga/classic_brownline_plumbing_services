aa30cd4a89753da7c00db2af1e280eab52b17d33
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-11-11 14:54:29 +0100

    service.js:

---
+++ classic_brownline_plumbing_services_website/src/components/service.js
@@ -0,0 +1,121 @@
+import { useEffect, useState } from "react";
+
+const serviceCardsSlides = [
+    {
+        id: 1,
+        icon: "fa-water",
+        title: "Drain Repair",
+        description: "Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam justo.",
+    },
+    {
+        id: 2,
+        icon: "fa-toilet",
+        title: "Toilet Pipe Repair",
+        description: "Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam justo.",
+    },
+    {
+        id: 3,
+        icon: "fa-shower",
+        title: "Sewer Line Repair",
+        description: "Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam justo.",
+    },
+    {
+        id: 4,
+        icon: "fa-tint",
+        title: "Water Heater Repair",
+        description: "Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam justo.",
+    },
+    {
+        id: 5,
+        icon: "fa-water",
+        title: "Drain Repair",
+        description: "Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam justo.",
+    },
+    {
+        id: 6,
+        icon: "fa-toilet",
+        title: "Toilet Pipe Repair",
+        description: "Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam justo.",
+    },
+    {
+        id: 7,
+        icon: "fa-shower",
+        title: "Sewer Line Repair",
+        description: "Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam justo.",
+    },
+    {
+        id: 8,
+        icon: "fa-tint",
+        title: "Water Heater Repair",
+        description: "Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam justo.",
+    },
+]
+
+function Service({renderServices, yearFounded, currentYear}) {
+	const [currentServiceCardIndex, setCurrentServiceCardIndex] = useState(0);
+    const totalServiceCards = serviceCardsSlides.length;
+    useEffect(() => {
+        const intervalTime = 1500; // match your CSS slide duration/pause timing
+        const interval = setInterval(() => {
+            setCurrentServiceCardIndex((prev) => (prev + 1) % totalServiceCards);
+        }, intervalTime);
+
+        return () => clearInterval(interval);
+    }, [totalServiceCards]);
+	return (
+		<div className={`container-fluid py-5 px-4 px-lg-0 ${renderServices ? '' : 'd-none'}`}>
+			<div className="row g-0">
+				<div className="col-lg-3 d-none d-lg-flex">
+					<div className="d-flex align-items-center justify-content-center bg-primary w-100 h-100">
+						<h1 className="display-3 text-white m-0 rotate-n90">{currentYear-yearFounded} Years Experience</h1>
+					</div>
+				</div>
+				<div className="col-md-12 col-lg-9">
+					<div className="ms-lg-5 ps-lg-5">
+						<div className="text-center text-lg-start wow fadeInUp" data-wow-delay="0.1s">
+							<h6 className="text-secondary text-uppercase">Our Services</h6>
+							<h1 className="mb-5">Explore Our Services</h1>
+						</div>
+						<div className="owl-carousel service-carousel position-relative wow fadeInUp" data-wow-delay="0.1s">
+							<div className="owl-stage-outer">
+								<div className="owl-stage service-card-slider"
+								style={{
+									transform: `translate3d(-${(currentServiceCardIndex - 2) * 372}px, 0, 0)`, // <-- move horizontally
+									transition: "transform 0.5s ease-in-out", // smooth animation
+								}}>
+									{serviceCardsSlides.map((serviceCard, scIdx) => {
+										// const isActive = scIdx % totalServiceCards === currentServiceCardIndex
+										return (
+											<div key={serviceCard.title+scIdx}
+											className="owl-item cloned">
+												<div className="bg-light p-4 border-radius-10">
+													<div className="d-flex align-items-center justify-content-center border border-5 border-white mb-4 w-75 h-75 border-radius-10">
+														<i className={`fa ${serviceCard.icon} fa-2x text-primary`}></i>
+													</div>
+													<h4 className="mb-3">{scIdx+1} {serviceCard.title}</h4>
+													<p>{serviceCard.description}</p>
+													<p className="text-primary fw-medium"><i className="fa fa-check text-success me-2"></i>Quality Service</p>
+													<p className="text-primary fw-medium"><i className="fa fa-check text-success me-2"></i>Customer Satisfaction</p>
+													<p className="text-primary fw-medium"><i className="fa fa-check text-success me-2"></i>Support 24/7</p>
+													<a href="##" className="btn bg-white text-primary w-100 mt-2 border-radius-5">Read More<i className="fa fa-arrow-right text-secondary ms-2"></i></a>
+												</div>
+											</div>
+										)
+									})}
+								</div>
+								<div className="owl-dots">
+									{serviceCardsSlides.map((_, _idx) => {
+										const isActive = _idx % totalServiceCards === currentServiceCardIndex
+										return (
+											<div key={_idx} className={`owl-dot ${isActive?'active':''}`}><span></span></div>
+									)})}
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+export { Service }
