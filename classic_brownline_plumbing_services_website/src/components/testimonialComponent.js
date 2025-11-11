cd59065793792dd63528057b3e11223119faa6ab
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-11-11 22:12:04 +0100

    testimonialComponent.js:

---
+++ classic_brownline_plumbing_services_website/src/components/testimonialComponent.js
@@ -0,0 +1,15 @@
+import { Testimonial } from "./modularComponents/testimonials";
+import { Booking } from "./modularComponents/booking";
+
+function TestimonialComponent() {
+	return (
+		<>
+			{/* Testimonial */}
+			<Testimonial />
+
+			{/* Booking */}
+			<Booking />
+		</>
+	);
+}
+export { TestimonialComponent }
