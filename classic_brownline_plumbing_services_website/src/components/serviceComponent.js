b037a838eb514dc3270f877f761732ce44d5c17d
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-11-13 22:32:23 +0100

    serviceComponent.js: recieved and passed handleNavigationScroll= to child component

--- classic_brownline_plumbing_services_website/src/components/serviceComponent.js
+++ classic_brownline_plumbing_services_website/src/components/serviceComponent.js
@@ -1,11 +1,13 @@
 import { Service } from "./modularComponents/service";
 import { Booking } from "./modularComponents/booking";
 
-function ServiceComponent({yearFounded, currentYear, activeSection}) {
+function ServiceComponent({handleNavigationScroll, yearFounded, currentYear, activeSection}) {
+	// console.log("Rendering ServiceComponent:", handleNavigationScroll);
 	return (
 		<>
 			{/* Service */}
 			<Service
+			handleNavigationScroll={handleNavigationScroll}
 			yearFounded={yearFounded}
 			currentYear={currentYear} />
 
