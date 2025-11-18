90a4b1a41bdb3f2ef2b02f9ebc4846386ad46091
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-11-18 01:36:06 +0100

    testimonialComponent.js: switched to react router

--- classic_brownline_plumbing_services_website/src/components/testimonialComponent.js
+++ classic_brownline_plumbing_services_website/src/components/testimonialComponent.js
@@ -1,9 +1,13 @@
 import { Testimonial } from "./modularComponents/testimonials";
 import { Booking } from "./modularComponents/booking";
+import { Pagerousel } from "./modularComponents/pagerousel";
 
 function TestimonialComponent() {
 	return (
 		<>
+			{/* Pagerousel */}
+			<Pagerousel />
+
 			{/* Testimonial */}
 			<Testimonial />
 
