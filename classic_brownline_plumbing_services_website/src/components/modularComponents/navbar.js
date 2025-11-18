2615acac24a4e075258c260f1105e2eb52f86e4d
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-11-18 01:35:58 +0100

    navbar.js: switched to react router

--- classic_brownline_plumbing_services_website/src/components/modularComponents/navbar.js
+++ classic_brownline_plumbing_services_website/src/components/modularComponents/navbar.js
@@ -1,8 +1,9 @@
 import { Fragment } from "react";
 import { titleCase } from "../../hooks/changeCase";
+import { Link, useLocation } from "react-router-dom";
 
 const headerOptions = [
-    {to: "home"},
+    {to: "/"},
 	{to: "about"},
 	{to: "services"},
     {to: "##"},
@@ -15,49 +16,50 @@ const pagesArray = [
     {to: "testimonials"},
 ]
 
-function Navbar({activeSection, handleNavigationScroll, isSticky}) {
+function Navbar({isSticky}) {
+	const currentPath = useLocation().pathname.split('/')[1];
 	return (
 		<div className={`container-fluid nav-bar bg-light ${isSticky ? 'sticky-top shadow' : ''}`} id="dynamic-navbar">
 			<nav className={`navbar navbar-expand-lg navbar-light app-bg-color p-3 py-lg-0 px-lg-4 ${isSticky ? '' : 'border-tl-radius-10 border-tr-radius-10'}`}>
-				<a href="##" className="navbar-brand d-flex align-items-center m-0 p-0 d-lg-none">
+				<Link to="/" className="navbar-brand d-flex align-items-center m-0 p-0 d-lg-none">
 					<h1 className="text-primary m-0 text-uppercase">Classic Brown</h1>
-				</a>
+				</Link>
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
 					<span className="fa fa-bars"></span>
 				</button>
 				<div className="collapse navbar-collapse" id="navbarCollapse">
 					<div className="navbar-nav me-auto">
 						{headerOptions.map((header, hIdx) => {
-							let isActive = header.to.toLowerCase() === activeSection.toLowerCase();
-							if (activeSection.toLowerCase() === '' && header.to.toLowerCase() === 'home') {
+							let isActive = header.to.toLowerCase() === currentPath.toLowerCase();
+							if (currentPath.toLowerCase() === '' && header.to.toLowerCase() === '/') {
 								isActive = true
 							}
 							return (
 								<div key={header.to+hIdx}
 								className="nav-item nav-link-container">
 									{header.to !== '##' ?
-										<a href="##"
-										onClick={(e)=>{
-											handleNavigationScroll(e, header.to);
-										}}
-										className={`nav-item nav-link ${isActive?'active':''}`}>{titleCase(header.to)}</a>
+										<Link to={header.to}
+										// onClick={(e)=>{
+										// 	handleNavigationScroll(e, header.to);
+										// }}
+										className={`nav-item nav-link ${isActive?'active':''}`}>{titleCase(header.to==='/'?'home':header.to)}</Link>
 										:
 										<div className="nav-item dropdown">
 											<span className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Sections</span>
 											<div className="dropdown-menu fade-up m-0 app-bg-color">
 												{pagesArray.map((page, pIdx) => {
-													isActive = page.to.toLowerCase() === activeSection.toLowerCase();
+													isActive = page.to.toLowerCase() === currentPath.toLowerCase();
 													// console.log({
-													//     page: page.to.toLowerCase(),
-													//     activeSection: activeSection.toLowerCase(),
-													//     isActive,
+													// 	page: page.to.toLowerCase(),
+													// 	activeSection: activeSection.toLowerCase(),
+													// 	isActive,
 													// })
 													return (
-														<a key={page.to+pIdx} href="##"
-														onClick={(e)=>{
-															handleNavigationScroll(e, page.to);
-														}}
-														className={`dropdown-item ${isActive?'active':''}`}>{titleCase(page.to)}</a>
+														<Link key={page.to+pIdx} to={page.to}
+														// onClick={(e)=>{
+														// 	handleNavigationScroll(e, page.to);
+														// }}
+														className={`dropdown-item ${isActive?'active':''}`}>{titleCase(page.to)}</Link>
 													)
 												})}
 											</div>
@@ -75,9 +77,9 @@ function Navbar({activeSection, handleNavigationScroll, isSticky}) {
 							<p className="mb-1 text-white">Emergency 24/7</p>
 							<h5 className="m-0 text-secondary">+012 345 6789</h5>
 						</div> */}
-						<a href="##" className={`navbar-brand m-0 p-0 ${isSticky ? '' : 'd-none d-lg-flex'}`}>
+						<Link to="/" className={`navbar-brand m-0 p-0 ${isSticky ? '' : 'd-none d-lg-flex'}`}>
 							<h2 className="text-primary m-0 text-uppercase">Classic Brown</h2>
-						</a>
+						</Link>
 					</div>
 				</div>
 			</nav>
