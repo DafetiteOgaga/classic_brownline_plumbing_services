644833b270dd3ee913d8c6dfa4d6ac3bcca28f84
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-11-18 01:37:14 +0100

    socials.js: handles socials

---
+++ classic_brownline_plumbing_services_website/src/components/modularComponents/socials.js
@@ -0,0 +1,22 @@
+
+
+const topbarSocials = [
+    {icon: "fa-facebook-f", link: "##"},
+    {icon: "fa-twitter", link: "##"},
+    {icon: "fa-linkedin-in", link: "##"},
+    {icon: "fa-instagram", link: "##"},
+	{icon: "fa-youtube", link: "##"},
+]
+
+function Socials({classStyle=null}) {
+	return (
+		<>
+			{topbarSocials.map((social, sIdx) => {
+				return (
+					<a key={social.icon+sIdx} className={classStyle} href={social.link}><i className={`fab ${social.icon}`}></i></a>
+				)
+			})}
+		</>
+	)
+}
+export { Socials }
